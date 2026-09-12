fdf457e0364e2f5aec4e5915866ebe00f06976e9
santialgorit08 <sant.algorit08@gmail.com>
2026-09-12 10:13:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+
+                            Online Python Compiler.
+         Codifique, compile, ejecute y depure el programa Python en línea.
+Escriba su código en este editor y presione el botón "Run" para ejecutarlo.
+
+'''
+
+print("Hola Mundo")
